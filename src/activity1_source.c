237fe53036f44fa8f28fe3cbc6ad490d6a092500
Asmita Kajrolkar <80444408+Asmita-Kajrolkar@users.noreply.github.com>
2021-04-26 10:17:47 +0530

    Update activity1_source.c

--- src/activity1_source.c
+++ src/activity1_source.c
@@ -1,1 +1,17 @@
-
+#include "user_utils.h"
+#include "activity1.h"
+void activity1(void)
+{
+    change_led_state(LED_OFF);
+    while(1){
+        /*Checks whether Button Sensor is ON or OFF */
+        if(!(PIND & 1<<PD0)){
+            /*Checks whether Heater button is ON or OFF */
+            if(!(PIND & 1<<PD1)){
+                change_led_state(LED_ON);
+            }
+            else change_led_state(LED_OFF);
+        }
+        else change_led_state(LED_OFF);
+    }
+}
