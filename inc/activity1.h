3ecdb505400576475d270cabfbc9c66ef7086120
Asmita Kajrolkar <80444408+Asmita-Kajrolkar@users.noreply.github.com>
2021-04-26 09:44:25 +0530

    Update activity1.h

--- inc/activity1.h
+++ inc/activity1.h
@@ -1,1 +1,40 @@
+#ifndef __ACTIVITY1_H_
+#define __ACTIVITY1_H_
+/**
+ * @file activity1.h
+ * @author Asmita Kajrolkar ()
+ * @brief Project to Blink an LED connected to AVR328 MCU GPIO Pin
+ * @version 0.1
+ * @date 2021-04-21
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
+/**
+ * Macro Definitions
+ */
+#define LED_ON 	(0x01)			/**< LED state HIGH */
+#define LED_OFF	(0x00)			/**< LED state LOW */
+
+#define LED_ON_TIME     (1000)  /**< LED ON time in milli seconds  */
+#define LED_OFF_TIME    (500)   /**< LED OFF time in milli seconds */
+
+/**
+ * Include files
+ */ 
+#include <avr/io.h>
+
+/**
+ * Function Definitions
+ */
+
+/**
+ * @brief Change the state of the LED Pin according to the value of state
+ * 
+ * @param state Pin level to which the LED Pin should be set
+ */
+void change_led_state(uint8_t state);
+
+
+#endif /** __ACTIVITY1_H_ */
