ddf73544ee6f7d448736f899228d1227076f9409
Asmita Kajrolkar <80444408+Asmita-Kajrolkar@users.noreply.github.com>
2021-04-30 19:48:35 +0530

    Update activity2.h

--- inc/activity2.h
+++ inc/activity2.h
@@ -1,1 +1,32 @@
+#ifndef _ACTIVITY2_H_
+#define _ACTIVITY2_H_
 
+/**
+ * @file activity2.h
+ * @author Asmita Kajrolkar ()
+ * @brief Activity2 in the embedded project
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+/**
+ * Macro definitions
+ * 
+ */
+#define CONVERSION_INCOMPLETE  ADCSRA & (1<<ADSC)
+/**
+ * @brief Function to initialize ADC
+ * 
+ */
+void InitializeADC();
+
+/**
+ * @brief Function to read values from a channel 
+ * @param[in] channel channel number
+ */
+uint16_t ReadADC(uint8_t channel);
+
+#endif /* _ACTIVITY2_H_ */
