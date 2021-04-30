b12b275cdcca7e1f006a35df2061370636de3e8a
Asmita Kajrolkar <80444408+Asmita-Kajrolkar@users.noreply.github.com>
2021-04-30 19:51:21 +0530

    Update activity4.h

--- inc/activity4.h
+++ inc/activity4.h
@@ -1,1 +1,39 @@
+#ifndef _ACTIVITY4_H_
+#define _ACTIVITY4_H_
 
+/**
+ * @file activity4.h
+ * @author Asmita Kajrolkar ()
+ * @brief Activity4 in the embedded project
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+/**
+ * Defining macros
+ * 
+ */
+#define BAUD 9600 /**< Baud Rate */
+#define BAUDRATE ((F_CPU)/(BAUD*16UL)-1) /**< set baud rate value for UBRR */
+#define DATA_TRANSMITTING !(UCSR0A & (1<<UDRE0)) /**< Data is transmitting */
+#define DATA_RECEIVING !(UCSR0A & (1<<RXC0)) /**< Data is receiving */
+#define DATA UDR0 /**< Data register */
+
+/**
+ * @brief Function to initialize UART 
+ * 
+ */
+void USARTInit(void);
+
+
+/**
+ * @brief Function to write string 
+ *  
+ * @param[in] letter single character
+ */
+void USARTWriteString(char letter);
+
+#endif  /* _ACTIVITY4_H_ */
