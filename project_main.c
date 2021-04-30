0f72c9581fa54d6ae7769d60518f50a6a0731711
Asmita Kajrolkar <80444408+Asmita-Kajrolkar@users.noreply.github.com>
2021-04-30 20:06:44 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -3,34 +3,16 @@
  * @author Asmita Kajrolkar ()
  * @brief Project to complete activities as described in Embedded track
  * @version 0.1
- * @date 2021-04-23
+ * @date 2021-04-30
  * 
  * @copyright Copyright (c) 2021
  * 
  */
-#include "project_config.h"
-#include "user_utils.h"
 #include "activity1.h"
-
-/**
- * @brief Initialize all the Peripherals and pin configurations
- * 
- */
-void peripheral_init(void)
-{
-	/* Configure LED Pin */
-	DDRB |= (1<<PB0);//First Pin of PORTB is used as output
-  DDRD = 0x00; //All Pins of PORTD are made input
-	PORTD |= (1<<PD1);
-	PORTD |= (1<<PD0);
-}
-
-void change_led_state(uint8_t state)
-{
-	LED_PORT = (state << LED_PIN);
-}
-
-
+#include "activity2.h"
+#include "activity3.h"
+#include "activity4.h"
+#define ON 1 /**< ON state */
 /**
  * @brief Main function where the code execution starts
  * 
@@ -40,9 +22,18 @@ void change_led_state(uint8_t state)
  */
 int main(void)
 {
-	/* Initialize Peripherals */
-	peripheral_init();
-	/* Activity1 */
-	activity1();
+	uint16_t Temperature, ADCchannel=0;
+	char TempType;
+	USARTInit(); /* Initialize Peripherals for UART */
+	while(1){
+		uint8_t Status;
+		/* Turns LED ON if and only if both switches ButtonSensor and Heater are closed */
+		Status=StatusLedActuator();
+		if(Status==ON){
+			Temperature=ReadADC(ADCchannel); /*reads sensor data from ADCChannel*/
+    	    TempType=GeneratePWM(Temperature);/*Generates PWM according to data received from sensor*/
+			USARTWriteString(TempType);/*Sends data to serial monitor*/
+		}
+	}
 	return 0;
 }
